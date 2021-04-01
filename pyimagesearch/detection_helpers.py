9b773e98aba1b1384d6f55e4289852d908f21b87
JayeMineM <72269561+JayeMineM@users.noreply.github.com>
2021-04-02 00:08:03 +0100

    Add files via upload

---
+++ pyimagesearch/detection_helpers.py
@@ -0,0 +1,9 @@
+# import the necessary packages
+import imutils
+
+def sliding_window(image, step, ws):
+    # slide a window across the image
+    for y in range(0, image.shape[0] - ws[1], step):
+        for x in range(0, image.shape[1] - ws[0], step):
+            # yield the current window
+            yield (x, y, image[y:y + ws[1], x:x + ws[0]])
